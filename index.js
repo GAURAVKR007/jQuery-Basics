059c9bf493e05792262ca808eb808120468d8470
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-24 12:21:50 +0530

    Added event listener

--- index.js
+++ index.js
@@ -3,8 +3,33 @@
 $("h1").addClass("big-title margin-50");  // Add classes with jQuery
 
 $("h1").text("Hello World");     // Add text in any attribute 
-$("h1").html("<em>Hello World</em>")  // Modify the innerHTML of the tag
+$("h1").html("<em>--</em>")  // Modify the innerHTML of the tag
 
-$("a").attr("href","https://duckduckgo.com/")  // Manipulating attributes with jQuery
+// $("a").attr("href","https://duckduckgo.com/")  // Manipulating attributes with jQuery
 
-$("a").attr("class","searchEngine");   // Adding class with attr because class is also an attribute
+// $("a").attr("class","searchEngine");   // Adding class with attr because class is also an attribute
+
+$("button.btn1").click(function(){
+    $("h1").css("color","black");
+});
+
+$("button.btn2").click(function(){
+    $("h1").css("color","White");
+});
+
+// $("button.btn-cls").click(function(){
+//     $("h1").text("--");
+//     $("h1").css("color","rgb(10, 150, 89)");
+// });
+
+
+$(document).keypress(function(event){
+     $("h1").text(event.key);
+});
+
+$("button.btn-cls").on("click",function(){     // On is An eventlistener method which take two parameter and listen for almost every event
+    $("h1").text("--");
+    $("h1").css("color","rgb(10, 150, 89)");
+});
+
+
