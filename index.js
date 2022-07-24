1c356192680fbb9f6d8df64d0071797730ffcb82
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-24 11:39:38 +0530

    Mainpulating Styles,Text,Attributes using jQuery

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-$("h1").css("color","red");
+// $("h1").css("color","red");  // Change styles with jQuery
+
+$("h1").addClass("big-title margin-50");  // Add classes with jQuery
+
+$("h1").text("Hello World");     // Add text in any attribute 
+$("h1").html("<em>Hello World</em>")  // Modify the innerHTML of the tag
+
+$("a").attr("href","https://duckduckgo.com/")  // Manipulating attributes with jQuery
+
+$("a").attr("class","searchEngine");   // Adding class with attr because class is also an attribute
